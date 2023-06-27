d4db3a05f489493852bf8a6ba770db1e2c4ef97b
deboraosantos <133919569+deboraosantos@users.noreply.github.com>
2023-06-26 21:02:40 -0300

    Add files via upload

---
+++ Unidade 3/Exercícios Unidade 3.R
@@ -0,0 +1,29 @@
+
+
+## Exercícios da Unidade 3.
+
+
+# 1. Escolha uma variável sobre sistema político (qualquer uma entre Q235 a Q239). Inverta os valores das respostas
+# de modo que 1 represente péssimo e 4 represente ótimo. Depois, mude os valores da variável de 1 a 4 para 0 a 1.
+
+# 2. Crie uma variável que separe os estados brasileiros (UF) nas cinco regiões geográficas do país.
+# Depois crie uma variável dummy para a região Sul.
+
+# 3. Escolha cinco variáveis do seu interesse e crie um subset para elas.
+
+# 4. Faça um gráfico que apresente a distribuição para a variável de satisfação com o sistema político (Q252).
+
+# 5. Faça um boxplot para 
+
+# 5. Faça um gráfico de barras para as médias de apoio à democracia (Q235) por ideologia (Q240). Lembre-se de inverter
+# os valores da variável de apoio à democracia antes de rodar o gráfico. Apresente-o na escala da variável.
+
+# 6. Faça um gráfico de dispersão com linha de regressão linear entre renda (Q288) e escolaridade (Q275).
+
+# 7. Escolha duas variáveis do seu interesse e faça uma correlação e uma regressão linear entre elas.
+
+# 8. Crie uma variável dummy para preocupação com uma guerra civil (Q148), em que 1 represente "Muito preocupado"
+# e "Preocupado". Depois, rode uma regressão logística em que a variável dummy seja a DV e as IVs sejam ideologia (Q240), 
+# gênero (Q260) e escolaridade (Q275).
+
+
